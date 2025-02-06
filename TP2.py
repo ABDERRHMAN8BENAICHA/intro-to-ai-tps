ffb69323cb193cb86471f6a10b0bd9b05ed06b2f
Ben aicha Abderrhmane <dahmangot2020@gmail.com>
2025-02-06 15:25:56 +0100

    final TP2

--- TP2.py
+++ TP2.py
@@ -1,5 +1,12 @@
-def fib(n):
-    a,b=0,1
-    while b < n:
-        print(b)
-        a,b = b,a+b
+my_classeur = {"negatifs":[],"positifs":[]}
+
+def trier(classeur,value):
+    if(value > 0):
+        classeur["positifs"].append(value)
+    else :
+        classeur["negatifs"].append(value)
+    return classeur
+trier(my_classeur,10)
+trier(my_classeur,-30)
+print("negatifs is : ",my_classeur["negatifs"])
+print("positifs is : ",my_classeur["positifs"])
