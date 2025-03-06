9a16d8a8b130f6e2b6f5820bf3807ea880c86ce9
ABDERRHMAN8BENAICHA <benaicha@gmail.com>
2025-03-06 11:47:25 +0100

    tp3 task 1 is ok

---
+++ TP3/main.py
@@ -0,0 +1,25 @@
+
+rules = [
+    {"if": ["A", "B"], "then": "C"},
+    {"if": ["P"], "then": "C"},
+    {"if": ["C", "L","D"], "then": "T"},
+]
+
+facts = [
+    "P",
+    "L",
+    "D",
+]
+
+
+def infer(rules, facts):
+    conclusions = set()
+
+    for rule in rules:
+        if all(condition in facts for condition in rule["if"]):
+            conclusions.add(rule["then"])
+
+    return conclusions
+
+new_conclusions = infer(rules, facts)
+print("the result is : ", new_conclusions)
